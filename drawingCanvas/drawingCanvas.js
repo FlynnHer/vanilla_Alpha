de131abd4febcca6ab8f7168a7fcd5f91f1bceda
gunguy <flynnher@gmail.com>
2022-12-27 17:13:00 +0900

    canvas : 클릭과 드래그로 선그리기 구현

--- drawingCanvas/drawingCanvas.js
+++ drawingCanvas/drawingCanvas.js
@@ -13,15 +13,37 @@ const colors = [
   "#2980b9"
 ] // https://flatuicolors.com/
 
-ctx.lineWidth = 2;    // 두께 지정
+ctx.lineWidth = 2;      // 두께 지정
+ctx.strokeStyle = "red";
+let isPainting = false; // 원점 지정 Flag
 
-function onClick(e) {
-  ctx.beginPath();
-  ctx.moveTo(0,0)     // 원점 고정하기
-  const color = colors[Math.floor(Math.random() *colors.length)];
-  ctx.strokeStyle = color;            // 색상 선택
-  ctx.lineTo(e.offsetX, e.offsetY);   // 클릭된 좌표(x, y) 저장
-  ctx.stroke();                       // 선 그리기
+// function onClick(e) {
+//   ctx.beginPath();
+//   ctx.moveTo(0,0)     // 원점 고정하기
+//   const color = colors[Math.floor(Math.random() *colors.length)];
+//   ctx.strokeStyle = color;            // 색상 선택
+//   ctx.lineTo(e.offsetX, e.offsetY);   // 클릭된 좌표(x, y) 저장
+//   ctx.stroke();                       // 선 그리기
+// }
+
+function onMouseMove(e) {
+  if(isPainting) {
+    ctx.lineTo(e.offsetX, e.offsetY);
+    ctx.stroke();
+    return;
+  }
+  ctx.moveTo(e.offsetX, e.offsetY);
+  // console.log("moveTo", e.offsetX, e.offsetY)
 }
-
-canvas.addEventListener("click", onClick);
+function onMouseDown(e) {
+  isPainting = true;
+  ctx.moveTo(e.offsetX, e.offsetY);
+  // console.log("moveDown", e.offsetX, e.offsetY)
+}
+function onMouseUp(e) {
+  isPainting = false;
+}
+// canvas.addEventListener("click", onClick);
+canvas.addEventListener("mousemove", onMouseMove);
+canvas.addEventListener("mousedown", onMouseDown)
+canvas.addEventListener("mouseup", onMouseUp)
