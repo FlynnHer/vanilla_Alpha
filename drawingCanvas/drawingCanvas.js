1d9cf6b85fd0efc10a2e47a6a345d47097bc1227
gunguy <flynnher@gmail.com>
2022-12-27 15:18:52 +0900

    fill & stroke : 사각형과 선 그리기로 집 만들어보기

--- drawingCanvas/drawingCanvas.js
+++ drawingCanvas/drawingCanvas.js
@@ -3,4 +3,13 @@ const ctx = canvas.getContext("2d");
 canvas.width = 800;
 canvas.height = 800;
 
-ctx.fillRect(100,100, 100,100);
+// 집 모양 만들어 보기
+ctx.fillRect(200,200, 50,300);
+ctx.fillRect(450,200, 50,300);
+ctx.strokeRect(325,400, 50,100);
+ctx.fillRect(200,200, 250,20);
+
+ctx.moveTo(200, 200);
+ctx.lineTo(350, 100);
+ctx.lineTo(500, 200);
+ctx.fill()
