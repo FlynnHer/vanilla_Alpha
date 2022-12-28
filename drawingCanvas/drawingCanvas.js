1c482bae92923e42215a97e0b2a9bda33e755a37
gunguy <flynnher@gmail.com>
2022-12-28 13:14:08 +0900

    기본 그림판 완성

--- drawingCanvas/drawingCanvas.js
+++ drawingCanvas/drawingCanvas.js
@@ -1,33 +1,27 @@
 const canvas = document.querySelector("canvas");
 const ctx = canvas.getContext("2d");
 const $lineWidth = document.getElementById("line-width");
-const $color = document.getElementById("color");
-canvas.width = 800;
-canvas.height = 800;
+ 
+// https://flatuicolors.com/
+// 배열로 캐스팅해서 선언
+const $color = document.getElementById("color")
+const $colorOptions = Array.from(document.getElementsByClassName("color-option"));
+const $btnMode = document.getElementById("mode-btn");
+const $btnClear = document.getElementById("clear-btn");
 
-const colors = [
-  "#e67e22",
-  "#f39c12",
-  "#d35400",
-  "#27ae60",
-  "#2ecc71",
-  "#2980b9",
-  "#2980b9"
-] // https://flatuicolors.com/
-
+//-- 초기화 ----------------------------
+const CANVAS_W = 800;
+const CANVAS_H= 800;
+const INIT_COLOR = "#000000";
+canvas.width = CANVAS_W;
+canvas.height = CANVAS_H;
 ctx.lineWidth = $lineWidth.value;      // 두께 지정
-ctx.strokeStyle = "red";
+ctx.strokeStyle = INIT_COLOR;
+ctx.fillStyle = INIT_COLOR;
 let isPainting = false; // 원점 지정 Flag
+let isFilling = false;  // 선/채우기 Flag
 
-// function onClick(e) {
-//   ctx.beginPath();
-//   ctx.moveTo(0,0)     // 원점 고정하기
-//   const color = colors[Math.floor(Math.random() *colors.length)];
-//   ctx.strokeStyle = color;            // 색상 선택
-//   ctx.lineTo(e.offsetX, e.offsetY);   // 클릭된 좌표(x, y) 저장
-//   ctx.stroke();                       // 선 그리기
-// }
-
+//-- Actions --------------------------
 function onMouseMove(e) {
   if(isPainting) {
     ctx.lineTo(e.offsetX, e.offsetY);
@@ -36,27 +30,56 @@ function onMouseMove(e) {
   }
   ctx.beginPath();
   ctx.moveTo(e.offsetX, e.offsetY);
-  // console.log("moveTo", e.offsetX, e.offsetY)
 }
 function onMouseDown(e) {
   isPainting = true;
   ctx.moveTo(e.offsetX, e.offsetY);
-  // console.log("moveDown", e.offsetX, e.offsetY)
 }
 function onMouseUp(e) {
   isPainting = false;
 }
-// canvas.addEventListener("click", onClick);
+function onMouseClick(e) {
+  if(isFilling) {
+    ctx.fillRect(0,0, CANVAS_W, CANVAS_H)
+  }
+}
+
 function onLineWidthChange(e) {
   ctx.lineWidth = e.target.value;
-  console.log(e.target.value);
 }
 function onColorChange(e) {
   ctx.strokeStyle = e.target.value;
+  ctx.fillStyle = e.target.value;
 }
+function onColorClick(e) {
+  ctx.strokeStyle = e.target.dataset.color; // 선 색상
+  ctx.fillStyle = e.target.dataset.color;   // 칠하기 색상
+  $color.value = e.target.dataset.color;    // 선택된 색상 표시
+}
+function btnModeClick(e) {
+  if(isFilling) {
+    isFilling = false;
+    $btnMode.innerText = "Fill (선 그리기)"
+  } else {
+    isFilling = true;
+    $btnMode.innerText = "Draw (채우기)"
+  }
+}
+function btnClearClick(e) {
+  ctx.fillStyle = "white";
+  ctx.fillRect(0,0, CANVAS_W, CANVAS_H);
+  $color.value = INIT_COLOR;
+  // ctx.stroke();
+}
+//-- attach Event Listener -----------
 canvas.addEventListener("mousemove", onMouseMove);
-canvas.addEventListener("mousedown", onMouseDown)
-canvas.addEventListener("mouseup", onMouseUp)
-canvas.addEventListener("mouseleave", onMouseUp)
-$lineWidth.addEventListener("change", onLineWidthChange)
-$color.addEventListener("change", onColorChange)
+canvas.addEventListener("mousedown", onMouseDown);
+canvas.addEventListener("mouseup", onMouseUp);
+canvas.addEventListener("click", onMouseClick);
+// canvas.addEventListener("mouseleave", onMouseUp);
+$lineWidth.addEventListener("change", onLineWidthChange);
+$color.addEventListener("change", onColorChange);
+$colorOptions.forEach(color => color.addEventListener("click", onColorClick));
+$btnMode.addEventListener("click", btnModeClick);
+$btnClear.addEventListener("click", btnClearClick);
+// console.log($colorOptions)
